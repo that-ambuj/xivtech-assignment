ef0164227d73cdf3072f018cf94eb29e33de4f83
that-ambuj <ambujs89@gmail.com>
2023-06-03 13:21:34 +0530

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+// import { useState } from "react";
+
+function App() {
+  // const [count, setCount] = useState(0);
+
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <div className="my-auto">
+        <h1 className="text-4xl font-semibold">Weather App</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
