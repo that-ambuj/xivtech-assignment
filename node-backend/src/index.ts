765655afdf5a52fdb3e708e5866e6fb17ef5c57f
that-ambuj <ambujs89@gmail.com>
2023-06-03 20:11:37 +0530

    implement weather api in the backend

--- node-backend/src/index.ts
+++ node-backend/src/index.ts
@@ -1,19 +1,66 @@
 import express from "express";
+import fetch from "node-fetch";
 import z from "zod";
+import dotenv from "dotenv";
+dotenv.config();
 
 const app = express();
 
-app.get("/api", (_, res) => {
-  res.send("hello");
-});
+const OPEN_WEATHER_API_KEY = process.env.OPEN_WEATHER_API_KEY;
+
+app.use(express.json());
 
 const WeatherRequestSchema = z.object({
   cities: z.array(z.string()).default([]),
 });
 
-// TODO: implement weather api
-app.post("/getWeather", async (req, res) => {
-  const { cities } = await WeatherRequestSchema.parseAsync(req);
+const WeatherData = z.object({
+  coord: z.object({
+    lon: z.number(),
+    lat: z.number(),
+  }),
+  main: z.object({
+    temp: z.number(),
+  }),
+  name: z.string(),
 });
+
+app.post("/api/getWeather", async (req, res) => {
+  if (!OPEN_WEATHER_API_KEY) {
+    res.status(500).json({ message: "Missing Open Weather API Key" });
+  }
+
+  const { cities } = await WeatherRequestSchema.parseAsync(req.body);
+  const weatherData = await Promise.all(cities.map(getWeatherByCity));
+
+  const weather = weatherData.reduce((acc: any, weather) => {
+    const { name, main } = weather;
+
+    if (!acc[name]) {
+      acc[name] = main.temp;
+    }
+
+    return acc;
+  }, {});
+
+  return res.json({ weather });
+});
+
+async function getWeatherByCity(
+  city: string
+): Promise<z.infer<typeof WeatherData>> {
+  const params = new URLSearchParams();
+  params.set("appid", OPEN_WEATHER_API_KEY as string);
+  params.set("q", city);
+  params.set("units", "metric");
+
+  const url = `https://api.openweathermap.org/data/2.5/weather?${params.toString()}`;
+
+  const res = await fetch(url);
+  const data = await res.json();
+
+  const parsedData = await WeatherData.parseAsync(data);
+  return parsedData;
+}
 
 export default app;
