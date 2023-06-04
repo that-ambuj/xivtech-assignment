8b816e2923aada25d74b438b0fee4f3b9e00bd20
that-ambuj <ambujs89@gmail.com>
2023-06-04 18:26:09 +0530

    fix express error handling

--- node-backend/src/index.ts
+++ node-backend/src/index.ts
@@ -12,7 +12,7 @@ const OPEN_WEATHER_API_KEY = process.env.OPEN_WEATHER_API_KEY;
 
 app.use(express.json());
 
-if (process.env.NODE_ENV?.match("dev")) {
+if (process.env.NODE_ENV?.match(/dev/i)) {
   app.use(cors());
 }
 
@@ -35,46 +35,63 @@ const WeatherData = z.object({
   name: z.string(),
 });
 
-app.post("/api/getWeather", async (req, res) => {
-  if (!OPEN_WEATHER_API_KEY) {
-    res.status(500).json({ message: "Missing Open Weather API Key" });
-  }
-
-  const { cities } = await WeatherRequestSchema.parseAsync(req.body);
-  const weatherData = await Promise.all(cities.map(getWeatherByCity));
-
-  const weather = weatherData.reduce((acc: any, weather) => {
-    const { name, main } = weather;
-
-    if (!acc[name]) {
-      acc[name] = main.temp;
+app.post("/api/getWeather", async (req, res, next) => {
+  try {
+    if (!OPEN_WEATHER_API_KEY) {
+      res.status(500).json({ message: "Missing Open Weather API Key" });
     }
 
-    return acc;
-  }, {});
+    const { cities } = WeatherRequestSchema.parse(req.body);
+    const weatherData = await Promise.all(cities.map(getWeatherByCity));
 
-  return res.json({ weather });
+    const weather = weatherData.reduce((acc: any, weather) => {
+      const { name, main } = weather;
+
+      if (!acc[name]) {
+        acc[name] = main.temp;
+      }
+
+      return acc;
+    }, {});
+
+    return res.json({ weather });
+  } catch (e) {
+    next(e);
+  }
 });
 
 async function getWeatherByCity(
   city: string
 ): Promise<z.infer<typeof WeatherData>> {
-  const params = new URLSearchParams();
-  params.set("appid", OPEN_WEATHER_API_KEY as string);
-  params.set("q", city);
-  params.set("units", "metric");
+  try {
+    const params = new URLSearchParams();
+    params.set("appid", OPEN_WEATHER_API_KEY as string);
+    params.set("q", city);
+    params.set("units", "metric");
 
-  const url = `https://api.openweathermap.org/data/2.5/weather?${params.toString()}`;
+    const url = `https://api.openweathermap.org/data/2.5/weather?${params.toString()}`;
 
-  const res = await fetch(url);
-  const data = await res.json();
+    const res = await fetch(url);
 
-  const parsedData = await WeatherData.parseAsync(data);
-  return parsedData;
+    if (res.status > 399) {
+      console.error(res.json());
+      throw new Error("Open API Error");
+    }
+
+    const data = await res.json();
+
+    const parsedData = WeatherData.parse(data);
+    return parsedData;
+  } catch (e) {
+    throw new Error(e as string);
+  }
 }
 
-// @ts-ignore
-const errorHandler: ErrorRequestHandler = (err, _, res) => {
+const errorHandler: ErrorRequestHandler = (err, req, res, next) => {
+  if (res.headersSent) {
+    return next(err);
+  }
+
   console.error(err);
   res.status(500).send({ message: err.message ?? "Something went wrong" });
 };
